d28a8047f5e9cc9d79eee98dd86e5fc6da69ab4e
Hana <momenthana@kakao.com>
2020-06-30 20:31:16 +0900

    Join Voice Channel

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const Discord = require('discord.js');
-const discord = new Discord.Client();
+const Discord = require('discord.js')
+const ytdl = require('ytdl-core');
+
+const discord = new Discord.Client()
 
 discord.on('ready', () => {
-  console.log(`Logged in as ${discord.user.tag}!`);
-});
+  console.log(`Logged in as ${discord.user.tag}!`)
+})
 
 discord.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+  if (msg.content.includes('play')) {
+    const voiceChannel = msg.member.voice.channel;
 
-discord.login(process.env.discordToken);
+    if (voiceChannel) {
+      voiceChannel.join()
+        .then(connection => {
+          connection
+            .play(ytdl('https://youtu.be/L8UUYfe6-UA'))
+            .on('finish', () => {
+              console.log('Finish')
+            })
+            .on('error', err => console.error(err))
+        })
+    }
+  }
+})
+
+discord.login(process.env.discordToken)
