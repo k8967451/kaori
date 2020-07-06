8bbd45b072b8ab4a5269d41ea07692846882db2c
Hana <momenthana@kakao.com>
2020-07-06 14:59:05 +0900

    다중 URL 처리

--- music.js
+++ music.js
@@ -41,20 +41,37 @@ const index = async msg => {
               queue: []
             }
           })
-          data[msg.guild.id].queue.push(ytdl.getVideoID(url[0]))
-          play(msg, embed)
+          for (const key in url) {
+            const id = ytdl.getVideoID(url[key])
+            data[msg.guild.id].queue.push(id)
+            if (key == 0) {
+              play(msg, embed)
+            } else {
+              const info = await ytdl.getInfo(id)
+              const embed = new Discord.MessageEmbed()
+                .setColor(process.env.color || '#f7cac9')
+                .setTimestamp()
+                .setFooter(msg.author.username, msg.author.avatarURL())
+                .setTitle(info.title)
+                .setURL(info.video_url)
+                .setDescription('음악을 대기열에 추가했어!')
+              msg.channel.send({ embed })
+            }
+          }
         } else if (msg.member.voice.channel != data[msg.guild.id].voiceChannel) {
           embed.setDescription('음악을 추가하려면 동일한 음성 채널에 있어야 해!')
           msg.channel.send({ embed })
         } else {
-          const id = ytdl.getVideoID(url[0])
-          data[msg.guild.id].queue.push(id)
-          const info = await ytdl.getInfo(id)
-          embed
-            .setTitle(info.title)
-            .setURL(info.video_url)
-            .setDescription('음악을 대기열에 추가했어!')
-          msg.channel.send({ embed })
+          url.forEach(async e => {
+            const id = ytdl.getVideoID(e)
+            data[msg.guild.id].queue.push(id)
+            const info = await ytdl.getInfo(id)
+            embed
+              .setTitle(info.title)
+              .setURL(info.video_url)
+              .setDescription('음악을 대기열에 추가했어!')
+            msg.channel.send({ embed })
+          })
         }
       } else {
         embed.setDescription('정확한 Youtube URL 주소를 보내줘!')
