d7869e8baa5c4bb72bccdbce2b7da0532bd99c42
Hana <momenthana@kakao.com>
2020-07-13 15:28:47 +0900

    Add volume set cmd

--- src/music.ts
+++ src/music.ts
@@ -10,7 +10,7 @@ const play = async (msg, embed) => {
     .setURL(info.video_url)
     .setImage(`https://img.youtube.com/vi/${info.video_id}/maxresdefault.jpg`)
   msg.channel.send({ embed })
-  const dispatcher = data[msg.guild.id].conn
+  data[msg.guild.id].conn
     .play(ytdl(id), { bitrate: 'auto' })
     .on('finish', () => {
       if (data[msg.guild.id].queue.length) {
@@ -21,7 +21,7 @@ const play = async (msg, embed) => {
       }
     })
     .on('error', err => console.error(err))
-  dispatcher.setVolume(0.5)
+  data[msg.guild.id].conn.dispatcher.setVolume(0.5)
 }
 
 const index = async msg => {
@@ -114,6 +114,34 @@ const index = async msg => {
         msg.channel.send({ embed })
       } else {
         embed.setDescription('음악 대기열이 없어!')
+        msg.channel.send({ embed })
+      }
+    } else {
+      embed.setDescription('음악이 재생중이지 않아!')
+      msg.channel.send({ embed })
+    }
+  }
+
+  if (msg.content.match(/volume/i)) {
+    if (data[msg.guild.id]) {
+      if (msg.member.voice.channel == data[msg.guild.id].voiceChannel) {
+        const number = Number(msg.content.replace(/[^{0-9.}]/gi, ''))
+        if (number) {
+          if (100 >= number && number >= 0) {
+            data[msg.guild.id].conn.dispatcher.setVolume(number / 100)
+            embed.setTitle('Volume')
+            embed.setDescription('음량을 ' + number + '으로 설정했어!')
+            msg.channel.send({ embed })
+          } else {
+            embed.setDescription('0에서 100 사이의 값을 입력해줘!')
+            msg.channel.send({ embed })
+          }
+        } else {
+          embed.setDescription('음량은 ' + data[msg.guild.id].conn.dispatcher.volume * 100 + '으로 설정되어 있어!')
+          msg.channel.send({ embed })
+        }
+      } else {
+        embed.setDescription('음량을 조절하려면 동일한 음성 채널에 있어야 해!')
         msg.channel.send({ embed })
       }
     } else {
