02166a8f5a9b3384ca13f5dcbb470f8016537ba0
Hana <momenthana@kakao.com>
2020-07-19 23:58:03 +0900

    Create remove.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,7 @@
 import leave from './leave'
 import play from './play'
 import queue from './queue'
+import remove from './remove'
 import skip from './skip'
 import volume from './volume'
 
@@ -8,6 +9,7 @@ export {
   leave,
   play,
   queue,
+  remove,
   skip,
   volume
 }
--- src/music.ts
+++ src/music.ts
@@ -1,5 +1,5 @@
 import discord from 'discord.js'
-import { leave, play, queue, skip, volume } from './commands'
+import { leave, play, queue, remove, skip, volume } from './commands'
 const data = {}
 
 const index = async msg => {
@@ -8,34 +8,12 @@ const index = async msg => {
     .setTimestamp()
     .setFooter(msg.author.username, msg.author.avatarURL())
 
-  play(msg, embed, data)
-  skip(msg, embed, data)
-  queue(msg, embed, data)
-  volume(msg, embed, data)
   leave(msg, embed, data)
-
-  if (msg.content.match(/(remove|delete|del)/i)) {
-    if (data[msg.guild.id]) {
-      if (data[msg.guild.id].queue.length) {
-        const number = Number(msg.content.replace(/[^{0-9}]/gi, ''))
-        if (number > 0) {
-          data[msg.guild.id].queue.splice(number - 1, 1)
-          embed.setTitle('Remove')
-          embed.setDescription('음악 대기열에서 삭제했어!')
-          msg.channel.send({ embed })
-        } else {
-          embed.setDescription('음악을 삭제하려면 대기열에 번호를 알려줘!')
-          msg.channel.send({ embed })
-        }
-      } else {
-        embed.setDescription('음악 대기열이 없어!')
-        msg.channel.send({ embed })
-      }
-    } else {
-      embed.setDescription('음악이 재생중이지 않아!')
-      msg.channel.send({ embed })
-    }
-  }
+  play(msg, embed, data)
+  queue(msg, embed, data)
+  remove(msg, embed, data)
+  skip(msg, embed, data)
+  volume(msg, embed, data)
 }
 
 export default index
