54403a5ded460ff1b79879b4d08c1678424223cc
Hana <momenthana@kakao.com>
2020-07-19 23:37:57 +0900

    Create play.ts

--- src/music.ts
+++ src/music.ts
@@ -1,28 +1,7 @@
 import discord from 'discord.js'
 import ytdl from 'ytdl-core'
+import { play } from './commands'
 const data = {}
-
-const play = async (msg, embed) => {
-  const id = data[msg.guild.id].queue.shift()
-  const info = await ytdl.getInfo(id)
-  embed
-    .setTitle(info.title)
-    .setURL(info.video_url)
-    .setImage(`https://img.youtube.com/vi/${info.video_id}/maxresdefault.jpg`)
-  msg.channel.send({ embed })
-  data[msg.guild.id].conn
-    .play(ytdl(id), { bitrate: 'auto' })
-    .on('finish', () => {
-      if (data[msg.guild.id].queue.length) {
-        play(msg, embed)
-      } else {
-        data[msg.guild.id].conn.disconnect()
-        data[msg.guild.id] = null
-      }
-    })
-    .on('error', err => console.error(err))
-  data[msg.guild.id].conn.dispatcher.setVolume(0.5)
-}
 
 const index = async msg => {
   const embed = new discord.MessageEmbed()
@@ -30,59 +9,7 @@ const index = async msg => {
     .setTimestamp()
     .setFooter(msg.author.username, msg.author.avatarURL())
 
-  if (msg.content.includes('play')) {
-    if (msg.member.voice.channel) {
-      const url = msg.content.match(/(http(s)?:\/\/)?(www.)?youtu(be|.be)?(\.com)?\/.+/gi)
-      if (url) {
-        if (!data[msg.guild.id]) {
-          await msg.member.voice.channel.join().then(conn => {
-            data[msg.guild.id] = {
-              voiceChannel: msg.member.voice.channel,
-              conn,
-              queue: []
-            }
-          })
-          for (const key in url) {
-            const id = ytdl.getVideoID(url[key])
-            data[msg.guild.id].queue.push(id)
-            if (key === '0') {
-              play(msg, embed)
-            } else {
-              const info = await ytdl.getInfo(id)
-              const embed = new discord.MessageEmbed()
-                .setColor(process.env.color || '#f7cac9')
-                .setTimestamp()
-                .setFooter(msg.author.username, msg.author.avatarURL())
-                .setTitle(info.title)
-                .setURL(info.video_url)
-                .setDescription('음악을 대기열에 추가했어!')
-              msg.channel.send({ embed })
-            }
-          }
-        } else if (msg.member.voice.channel != data[msg.guild.id].voiceChannel) {
-          embed.setDescription('음악을 추가하려면 동일한 음성 채널에 있어야 해!')
-          msg.channel.send({ embed })
-        } else {
-          url.forEach(async e => {
-            const id = ytdl.getVideoID(e)
-            data[msg.guild.id].queue.push(id)
-            const info = await ytdl.getInfo(id)
-            embed
-              .setTitle(info.title)
-              .setURL(info.video_url)
-              .setDescription('음악을 대기열에 추가했어!')
-            msg.channel.send({ embed })
-          })
-        }
-      } else {
-        embed.setDescription('정확한 Youtube URL 주소를 보내줘!')
-        msg.channel.send({ embed })
-      }
-    } else {
-      embed.setDescription('음악을 재생하려면 음성 채널에 있어야 해!')
-      msg.channel.send({ embed })
-    }
-  }
+  play(msg, embed, data)
 
   if (msg.content.includes('skip')) {
     if (data[msg.guild.id]) {
