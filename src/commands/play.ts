3365c174e783a9d78886b5dc828daadec3b8cf6a
Hana <momenthana@kakao.com>
2020-08-12 22:30:29 +0900

    Create player.ts

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,25 +1,5 @@
 import ytdl from 'ytdl-core'
-
-const player = async (msg, embed, data) => {
-  const id = data[msg.guild.id].queue.shift()
-  const info = await ytdl.getInfo(id)
-  embed.setTitle(info.videoDetails.title)
-    .setURL(info.videoDetails.video_url)
-    .setImage(`https://img.youtube.com/vi/${info.videoDetails.videoId}/maxresdefault.jpg`)
-  msg.channel.send(embed)
-  data[msg.guild.id].conn
-    .play(ytdl(id), { bitrate: 'auto' })
-    .on('finish', () => {
-      if (data[msg.guild.id].queue.length) {
-        player(msg, embed, data)
-      } else {
-        data[msg.guild.id].conn.disconnect()
-        data[msg.guild.id] = null
-      }
-    })
-    .on('error', err => console.error(err))
-  data[msg.guild.id].conn.dispatcher.setVolume(0.5)
-}
+import { player } from '../utils'
 
 const play = async (msg, embed, data) => {
   if (msg.member.voice.channel) {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
 import embed from './embed'
 import load from './load'
+import player from './player'
 import save from './save'
 
 export {
   embed,
   load,
+  player,
   save
 }
