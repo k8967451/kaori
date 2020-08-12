7601e8d70badd59224628414930611304242a70a
Hana <momenthana@kakao.com>
2020-08-12 23:48:56 +0900

    검색하여 선택한 음악 재생

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,10 +1,12 @@
-const ytsr = require('ytsr')
+import ytdl from 'ytdl-core'
+import ytsr from 'ytsr'
+import { embed, player } from '../utils'
 const searchData = {}
 
-const search = (msg, embed) => {
+const search = (msg, Embed, data) => {
   if (!msg.content.replace(/kaori|search| /gi, '')) {
-    embed.setDescription('검색 키워드를 입력해줘!')
-    msg.channel.send(embed)
+    Embed.setDescription('검색 키워드를 입력해줘!')
+    msg.channel.send(Embed)
     return
   }
 
@@ -16,11 +18,11 @@ const search = (msg, embed) => {
       nextpageRef: filter.ref,
     }, (err, res) => {
       if (err) throw err
-      embed.setTitle('Search!')
+      Embed.setTitle('Search!')
         .setDescription(res.query + '에 대한 검색 결과')
       for (const key in res.items) {
-        const element = res.items[key]
-        embed.addField(`${Number(key) + 1}. ${element.title}`, element.live ? 'Live' : element.duration)
+        const item: any = res.items[key]
+        Embed.addField(`${Number(key) + 1}. ${item.title}`, item.live ? 'Live' : item.duration)
       }
 
       searchData[msg.channel.id] = res.items
@@ -30,18 +32,42 @@ const search = (msg, embed) => {
         return iconList.includes(reaction.emoji.name) && user.id === msg.author.id
       }
 
-      msg.channel.send(embed).then(reply => {
+      msg.channel.send(Embed).then(reply => {
         iconList.forEach(element => {
           reply.react(element)
         })
         reply.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-          .then(collected => {
+          .then(async collected => {
             const reaction = collected.first()
 
             for (const key in iconList) {
-              const element = iconList[key]
-              if (reaction.emoji.name === element) {
-                console.log(searchData[msg.channel.id][key])
+              const icon = iconList[key]
+              if (reaction.emoji.name === icon) {
+                const initEmbed = embed(msg)
+                const selected = searchData[msg.channel.id][key]
+                if (!data[msg.guild.id]) {
+                  await msg.member.voice.channel.join().then(conn => {
+                    data[msg.guild.id] = {
+                      voiceChannel: msg.member.voice.channel,
+                      conn,
+                      queue: []
+                    }
+                  })
+                  const id = ytdl.getVideoID(selected.link)
+                  data[msg.guild.id].queue.push(id)
+                  player(msg, embed(msg), data)
+                } else if (msg.member.voice.channel != data[msg.guild.id].voiceChannel) {
+                  initEmbed.setDescription('음악을 추가하려면 동일한 음성 채널에 있어야 해!')
+                  msg.channel.send(initEmbed)
+                } else {
+                  const id = ytdl.getVideoID(selected.link)
+                  data[msg.guild.id].queue.push(id)
+                  const info = await ytdl.getInfo(id)
+                  initEmbed.setTitle(info.videoDetails.title)
+                    .setURL(info.videoDetails.video_url)
+                    .setDescription('음악을 대기열에 추가했어!')
+                  msg.channel.send(initEmbed)
+                }
               }
             }
           })
