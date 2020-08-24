e793b34b505a766f01b112041398c18d44d69c8a
Hana <momenthana@kakao.com>
2020-08-24 18:06:50 +0900

    queue 코드 리팩토링

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -1,22 +1,22 @@
-import ytdl from 'ytdl-core'
-
 const queue = async (msg, embed, data) => {
-  if (data[msg.guild.id]) {
-    if (data[msg.guild.id].queue.length) {
-      embed.setTitle('Queue')
-      for (const key in data[msg.guild.id].queue) {
-        const e = data[msg.guild.id].queue[key]
-        embed.addField(`${Number(key) + 1}. ${e.title}`, `${e.duration} <@${e.msg.author.id}>님이 추가함`)
-      }
-      msg.channel.send(embed)
-    } else {
-      embed.setDescription('음악 대기열이 없어!')
-      msg.channel.send(embed)
-    }
-  } else {
+  if (!data[msg.guild.id]) {
     embed.setDescription('음악이 재생중이지 않아!')
     msg.channel.send(embed)
+    return
   }
+
+  if (!data[msg.guild.id].queue.length) {
+    embed.setDescription('음악 대기열이 없어!')
+    msg.channel.send(embed)
+    return
+  }
+
+  embed.setTitle('Queue')
+  for (const key in data[msg.guild.id].queue) {
+    const e = data[msg.guild.id].queue[key]
+    embed.addField(`${Number(key) + 1}. ${e.title}`, `${e.duration} <@${e.msg.author.id}>님이 추가함`)
+  }
+  msg.channel.send(embed)
 }
 
 export default queue
