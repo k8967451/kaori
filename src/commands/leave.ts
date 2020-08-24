8181425f6748dd94b9a6c22470ad111e75031b84
Hana <momenthana@kakao.com>
2020-08-24 19:48:38 +0900

    leave 코드 리팩토링

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -1,14 +1,15 @@
 const leave = (msg, embed, data) => {
-  if (data[msg.guild.id]) {
-    data[msg.guild.id].voiceChannel.leave()
-    delete data[msg.guild.id]
-    embed.setTitle('Leave')
-      .setDescription('음성 채널을 나왔어!')
-    msg.channel.send(embed)
-  } else {
+  if (!data[msg.guild.id]) {
     embed.setDescription('음악이 재생중이지 않아!')
     msg.channel.send(embed)
+    return
   }
+
+  data[msg.guild.id].voiceChannel.leave()
+  data[msg.guild.id] = null
+  embed.setTitle('Leave')
+    .setDescription('음성 채널을 나왔어!')
+  msg.channel.send(embed)
 }
 
 export default leave
